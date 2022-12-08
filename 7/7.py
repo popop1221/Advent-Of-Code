bb02317fcab1f3c9a6196d82710e52c5d6de0927
Kévin Lubert <kevin.lubert@epita.fr>
2022-12-08 06:26:45 +0100

    Day 7 and 8

--- 7/7.py
+++ 7/7.py
@@ -1,8 +1,120 @@
+class ArbreBinaire:
+    def __init__(self, valeur, name, parent):
+        self.valeur = valeur
+        self.parent = parent
+        self.name = name
+        self.node = []
+
+    def insert(self, valeur, name):
+        for node in self.node:
+            if node.name == name:
+                node.valeur = valeur
+        self.node.append(ArbreBinaire(valeur, name, self))
+
+    def update_tree(self):
+        result = self.valeur
+        for node in self.node:
+            node.update_tree()
+            result += node.valeur
+        self.valeur = result
+
+    def get_child(self, Name):
+        for node in self.node:
+            if node.name == Name:
+                return node
+        self.node.append(ArbreBinaire(0, Name, self))
+        return self.get_child(Name)
+
+
+    def bigger(self):
+        return max([self.valeur] + [i.valeur for i in self.node])
+
+    def get_valeur(self):
+        return self.valeur
+
+    def get_parent(self):
+        return self.parent
+
+    def calc(self):
+        result = 0
+        for ch in self.node:
+            if ch.valeur <= 100000 and ch.node != []:
+                result += ch.valeur
+            result += ch.calc()
+        return result
+
+    def ola(self, value):
+        result = []
+        for ch in self.node:
+            if (value + ch.valeur) >= 30000000 and ch.node != []:
+                result += [ch.valeur]
+                result += ch.ola(value)
+        return result
+
+
 with open('input.txt') as f:
     lines = f.readlines()
 
+tree = ArbreBinaire(0, "/", None)
+
+test_input = [
+"$ ls",
+"dir a",
+"14848514 b.txt",
+"8504156 c.dat",
+"dir d",
+"$ cd a",
+"$ ls",
+"dir e",
+"29116 f",
+"2557 g",
+"62596 h.lst",
+"$ cd e",
+"$ ls",
+"584 i",
+"$ cd ..",
+"$ cd ..",
+"$ cd d",
+"$ ls",
+"4060174 j",
+"8033020 d.log",
+"5626152 d.ext",
+"7214296 k"]
+
+result = 0
+cd_name = ""
+
+current_node = tree
+
+for line in lines:
+#for line in test_input:
+    line.replace("\n", "")
+
+    if line.startswith("$ cd"):
+        if line.startswith("$ cd .."):
+            current_node = current_node.parent
+            continue
+        cd_name = line.split(" ")[2]
+        current_node.insert(0, cd_name)
+        current_node = current_node.get_child(cd_name)
+        continue
+
+    if line.startswith("$ ls"):
+        continue
+
+    if line.startswith("dir"):
+        current_node.insert(0, line.split(" ")[1])
+        continue
+
+    temp = line.split(" ")
+    current_node.insert(int(temp[0]), temp[1])
 
 
 
+tree.update_tree()
+print(tree.bigger())
+print(tree.calc())
+print(tree.ola(70000000 - tree.valeur))
+print(min(tree.ola(70000000 - tree.valeur)))
 
 print("----- PART 2 -----")
--- 8/8.py
+++ 8/8.py
@@ -1,8 +1,150 @@
 with open('input.txt') as f:
     lines = f.readlines()
 
+test_input = ["30373",
+              "25512",
+              "65332",
+              "33549",
+              "35390"]
+
+inputs = []
 
 
+def test_visibility(map, x, y):
+    maxv = map[x][y]
+    visible = True
+    currentx = x
+    while (currentx - 1) >= 0:
+        currentx -= 1
+        if map[currentx][y] >= maxv:
+            visible = False
+            break
+    if visible:
+        return 1
 
+    visible = True
+    currentx = x
+    while (currentx + 1) < len(map):
+        currentx += 1
+        if map[currentx][y] >= maxv:
+            visible = False
+            break
+    if visible:
+        return 1
+
+    visible = True
+    currenty = y
+    while (currenty - 1) >= 0:
+        currenty -= 1
+        if map[x][currenty] >= maxv:
+            visible = False
+            break
+    if visible:
+        return 1
+
+    visible = True
+    currenty = y
+    while (currenty + 1) < len(map[0]):
+        currenty += 1
+        if map[x][currenty] >= maxv:
+            visible = False
+            break
+    if visible:
+        return 1
+    return 0
+
+
+result = []
+for line in lines:
+    # for line in test_input:
+    temp = []
+    line.replace("\n", "")
+    for char in line:
+        if char.replace("\n", "") != "":
+            temp.append(int(char.replace("\n", "")))
+    inputs.append(temp)
+
+for x in range(len(inputs)):
+    temp = []
+    for y in range(len(inputs[x])):
+        temp.append(test_visibility(inputs, x, y))
+    result.append(temp)
+
+result1 = 0
+for tab in result:
+    result1 += tab.count(1)
+print(result1)
 
 print("----- PART 2 -----")
+
+test_input = ["30373",
+              "25512",
+              "65332",
+              "33549",
+              "35390"]
+
+inputs = []
+
+
+def test_visibility2(map, x, y):
+    score = 1
+
+    maxv = map[x][y]
+    currentx = x
+    tempscore = 0
+    while (currentx - 1) >= 0:
+        currentx -= 1
+        tempscore += 1
+        if map[currentx][y] >= maxv:
+            break
+    score *= tempscore
+
+    currentx = x
+    tempscore = 0
+    while (currentx + 1) < len(map):
+        currentx += 1
+        tempscore += 1
+        if map[currentx][y] >= maxv:
+            break
+    score *= tempscore
+
+    currenty = y
+    tempscore = 0
+    while (currenty - 1) >= 0:
+        currenty -= 1
+        tempscore += 1
+        if map[x][currenty] >= maxv:
+            break
+    score *= tempscore
+
+    currenty = y
+    tempscore = 0
+    while (currenty + 1) < len(map[0]):
+        currenty += 1
+        tempscore += 1
+        if map[x][currenty] >= maxv:
+            break
+    score *= tempscore
+    return score if score > 0 else 1
+
+
+result = []
+for line in lines:
+    # for line in test_input:
+    temp = []
+    line.replace("\n", "")
+    for char in line:
+        if char.replace("\n", "") != "":
+            temp.append(int(char.replace("\n", "")))
+    inputs.append(temp)
+
+for x in range(len(inputs)):
+    temp = []
+    for y in range(len(inputs[x])):
+        temp.append(test_visibility2(inputs, x, y))
+    result.append(temp)
+
+result1 = 0
+for tab in result:
+    result1 = max([result1] + tab)
+print(result1)
