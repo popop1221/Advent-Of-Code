d49d71326429e3cd32d7cc5b03918033fcebe3b6
popop1221 <kevin.lubert12@yahoo.com>
2023-12-15 06:30:47 +0100

    [2023] Finaly a day without segfault or problems (Day 15)

--- 2015/8/8.py
+++ 2015/8/8.py
@@ -1,19 +1,24 @@
-import sys
-sys.setrecursionlimit(150000)
-
 with open('input.txt') as f:
     real_input = f.readlines()
 
 with open('test_input.txt') as f:
     test_input = f.readlines()
 
-
 inputt = real_input
 
+result = 0
 for line in inputt:
     line = line.replace("\n", "")
+    result += len(line) - len(eval(line))
+
+print(result)
 
 print("----- PART 2 -----")
 
+result2 = 0
 for line in inputt:
-    pass
+    line = line.replace("\n", "")
+    result2 += len(line) + line.count("\"") + line.count("\\") + 2
+    result2 -= len(eval(line))
+
+print(result2 - result)
--- 2023/15/15.py
+++ 2023/15/15.py
@@ -4,6 +4,48 @@ with open('input.txt') as f:
 with open('test_input.txt') as f:
     test_input = f.readlines()
 
+inputt = real_input
+inputt = [line.replace("\n", "") for line in inputt][0]
 
+
+def hash(name):
+    curr = 0
+    for char in name:
+        curr += ord(char)
+        curr *= 17
+        curr %= 256
+    return curr
+
+
+result = 0
+for line in inputt.split(","):
+    result += hash(line)
+
+print(result)
+print(hash("HASH"))
 
 print("----- PART 2 -----")
+boxes = [[] for _ in range(256)]
+
+for line in inputt.split(","):
+    hash_value = hash(line.split("=")[0].replace("-", ""))
+    if "-" in line:
+        index = 0
+        for t in range(len(boxes[hash_value])):
+            if boxes[hash_value][t][0] == line.replace("-", ""):
+                boxes[hash_value].pop(t)
+                break
+    else:
+        for t in range(len(boxes[hash_value])):
+            if boxes[hash_value][t][0] == line.split("=")[0]:
+                boxes[hash_value][t][1] = line.split("=")[1]
+                break
+        else:
+            boxes[hash_value].append(line.split("="))
+
+print(boxes)
+result = 0
+for box in range(len(boxes)):
+    for pos in range(len(boxes[box])):
+        result += (box + 1) * (pos + 1) * int(boxes[box][pos][1])
+print(result)
