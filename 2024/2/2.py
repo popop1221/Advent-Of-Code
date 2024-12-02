2eed86ef26520f29f594cc6f07a649e4e4b16826
popop1221 <kevin.lubert12@yahoo.com>
2024-12-02 06:14:57 +0100

    [2024] Day 2

--- 2024/2/2.py
+++ 2024/2/2.py
@@ -11,10 +11,46 @@ with open('test_input.txt') as f:
 inputt = real_input
 inputt = [line.replace("\n", "") for line in inputt]
 
+result = 0
 for line in inputt:
-    pass
+    linesp = [int(i) for i in line.split(" ")]
+    if linesp[0] < linesp[1]:
+        linesp.reverse()
+    prev = linesp[0]
+    print(linesp)
+    for i in linesp[1::]:
+        delta = prev - i
+        prev = i
+        if delta < 1 or delta > 3:
+            print(f"not safe for {i}  : {prev}")
+            break
+    else:
+        print("safe")
+        result += 1
+
+print(result)
 
 print("----- PART 2 -----")
 
+result = 0
 for line in inputt:
-    pass
+    len_line = len(line.split(" "))
+    for brute_force in range(len_line):
+        linesp = [int(i) for i in line.split(" ")]
+        if brute_force < len_line:
+            linesp.pop(brute_force)
+        if linesp[0] < linesp[1]:
+            linesp.reverse()
+        prev = linesp[0]
+        print(linesp)
+        for i in linesp[1::]:
+            delta = prev - i
+            prev = i
+            if delta < 1 or delta > 3:
+                print(f"unsafe for {i}  {delta}")
+                break
+        else:
+            result += 1
+            break
+
+print(result)
