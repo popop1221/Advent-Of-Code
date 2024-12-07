ac529c0a5a89bca516e5ba16b7f064d6f5f5d5a2
popop1221 <kevin.lubert12@yahoo.com>
2024-12-07 06:33:26 +0100

    [2024] Day 7

--- 2024/7/7.py
+++ 2024/7/7.py
@@ -11,10 +11,45 @@ with open('test_input.txt') as f:
 inputt = real_input
 inputt = [line.replace("\n", "") for line in inputt]
 
+
+def evaluate(needed, act, pos, numbs):
+    if act == needed:
+        return True
+    if act > needed or pos >= len(numbs):
+        return False
+    return evaluate(needed, act + numbs[pos], pos + 1, numbs) or evaluate(needed, act * numbs[pos], pos + 1, numbs)
+
+
+result = 0
 for line in inputt:
-    pass
+    sp = line.split(": ")
+    print(sp)
+    needed = int(sp[0].replace(":", ""))
+    numbers = [int(num) for num in sp[1].split(" ")]
+    print(numbers)
+    if evaluate(needed, numbers[0], 1, numbers):
+        result += needed
+print(result)
 
 print("----- PART 2 -----")
 
+
+def evaluate_2(needed, act, pos, numbs):
+    if act == needed:
+        return True
+    if act > needed or pos >= len(numbs):
+        return False
+
+    return evaluate_2(needed, act + numbs[pos], pos + 1, numbs) \
+           or evaluate_2(needed, int(str(act) + str(numbs[pos])), pos + 1, numbs) \
+           or evaluate_2(needed, act * numbs[pos], pos + 1, numbs)
+
+
+result = 0
 for line in inputt:
-    pass
+    sp = line.split(": ")
+    needed = int(sp[0].replace(":", ""))
+    numbers = [int(num) for num in sp[1].split(" ")]
+    if evaluate_2(needed, numbers[0], 1, numbers):
+        result += needed
+print(result)
