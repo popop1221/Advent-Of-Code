decf90021964f7d6ca014cf6884f9fc67c716650
popop1221 <kevin.lubert12@yahoo.com>
2024-12-10 06:18:44 +0100

    [2024] Day 10

--- 2024/10/10.py
+++ 2024/10/10.py
@@ -11,10 +11,61 @@ with open('test_input.txt') as f:
 inputt = real_input
 inputt = [line.replace("\n", "") for line in inputt]
 
-for line in inputt:
-    pass
+dir = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+
+def evaluate(marked, x, y):
+    # print(f"eval rec {x} {y}")
+
+    if (x, y) not in marked and inputt[x][y] == "9":
+        print(f"valid at {x} {y}")
+        marked.append((x, y))
+        return 1
+    res = 0
+    marked.append((x, y))
+    for next_pos in dir:
+        if x + next_pos[0] < 0 or y + next_pos[1] < 0 or x + next_pos[0] >= len(inputt) or y + next_pos[1] >= len(
+                inputt):
+            continue
+        if (x + next_pos[0], y + next_pos[1]) in marked:
+            continue
+        if inputt[x + next_pos[0]][y + next_pos[1]] == "." or int(inputt[x + next_pos[0]][y + next_pos[1]]) != int(
+                inputt[x][y]) + 1:
+            continue
+        res += evaluate(marked, x + next_pos[0], y + next_pos[1])
+    return res
+
+
+result = 0
+for i in range(len(inputt)):
+    for j in range(len(inputt[0])):
+        if inputt[i][j] == "0":
+            result += evaluate([], i, j)
+
+print(result)
 
 print("----- PART 2 -----")
 
-for line in inputt:
-    pass
+
+def evaluate_2(x, y):
+    if inputt[x][y] == "9":
+        return 1
+    res = 0
+    for next_pos in dir:
+        if x + next_pos[0] < 0 or y + next_pos[1] < 0 or x + next_pos[0] >= len(inputt) or y + next_pos[1] >= len(
+                inputt):
+            continue
+        if inputt[x + next_pos[0]][y + next_pos[1]] == "." or int(inputt[x + next_pos[0]][y + next_pos[1]]) != int(
+                inputt[x][y]) + 1:
+            continue
+        res += evaluate_2(x + next_pos[0], y + next_pos[1])
+    return res
+
+
+result = 0
+for i in range(len(inputt)):
+    for j in range(len(inputt[0])):
+        if inputt[i][j] == "0":
+            result += evaluate_2(i, j)
+
+print(result)
