a9aefe014398e5cb194a112e1e0d8389fd4a50bd
popop1221 <kevin.lubert12@yahoo.com>
2024-12-18 06:31:54 +0100

    [2024] Day 18

--- 2024/18/18.py
+++ 2024/18/18.py
@@ -11,10 +11,59 @@ with open('test_input.txt') as f:
 inputt = real_input
 inputt = [line.replace("\n", "") for line in inputt]
 
-for line in inputt:
-    pass
+max_coord = 71
+to_fall = 1024
+
+parsed = set()
+for i in range(to_fall):
+    sp = inputt[i].split(",")
+    parsed.add((int(sp[0]), int(sp[1])))
+
+for i in range(max_coord):
+    line = ""
+    for j in range(max_coord):
+        if (j, i) in parsed:
+            line += "#"
+        else:
+            line += "."
+    print(line)
+
+queue = [((0, 0), 0)]
+marked = []
+while queue:
+    elem = queue.pop(0)
+    if elem[0] == (max_coord - 1, max_coord - 1):
+        print(elem)
+        break
+    for (dx, dy) in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+        new_pos = (elem[0][0] + dx, elem[0][1] + dy)
+        if new_pos not in marked and new_pos[0] >= 0 and new_pos[1] >= 0 and new_pos[0] < max_coord and new_pos[
+            1] < max_coord and new_pos not in parsed:
+            marked.append(new_pos)
+            queue.append((new_pos, elem[1] + 1))
 
 print("----- PART 2 -----")
 
-for line in inputt:
-    pass
+for i in range(1024, len(inputt)):
+    print(f"Add {i}")
+    sp = inputt[i].split(",")
+    parsed.add((int(sp[0]), int(sp[1])))
+
+    queue = [((0, 0), 0)]
+    marked = []
+    found = False
+    while queue:
+        elem = queue.pop(0)
+        if elem[0] == (max_coord - 1, max_coord - 1):
+            found = True
+            break
+        for (dx, dy) in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            new_pos = (elem[0][0] + dx, elem[0][1] + dy)
+            if new_pos not in marked and new_pos[0] >= 0 and new_pos[1] >= 0 and new_pos[0] < max_coord and new_pos[
+                1] < max_coord and new_pos not in parsed:
+                marked.append(new_pos)
+                queue.append((new_pos, elem[1] + 1))
+    if not found:
+        print("--------------")
+        print(inputt[i])
+        print("--------------")
