7dd1a75fb1c7ccb296c23c84aeea17c54ae2cd8d
popop1221 <kevin.lubert12@yahoo.com>
2024-12-19 07:14:45 +0100

    [2024] DAy 19

--- 2024/19/19.py
+++ 2024/19/19.py
@@ -11,10 +11,48 @@ with open('test_input.txt') as f:
 inputt = real_input
 inputt = [line.replace("\n", "") for line in inputt]
 
-for line in inputt:
-    pass
+parsed = inputt[0].split(", ")
+parsed.sort(reverse=True)
+print(parsed)
+
+memo = {}
+def evaluate(word):
+    if word in memo.keys():
+        return memo[word]
+    for first in parsed:
+        if word.startswith(first):
+            if word[len(first):] == "":
+                return True
+            if evaluate(word[len(first):]):
+                memo[word] = True
+                return True
+    memo[word] = False
+    return False
+
+result = 0
+for line in inputt[2:]:
+    result += evaluate(line)
+
+print(result)
 
 print("----- PART 2 -----")
 
-for line in inputt:
-    pass
+memo2 = {}
+def evaluate2(word):
+    if word in memo2.keys():
+        return memo2[word]
+    res = 0
+    for first in parsed:
+        if word.startswith(first):
+            if word[len(first):] == "":
+                res += 1
+            else:
+                res += evaluate2(word[len(first):])
+    memo2[word] = res
+    return res
+
+result = 0
+for line in inputt[2:]:
+    result += evaluate2(line)
+
+print(result)
